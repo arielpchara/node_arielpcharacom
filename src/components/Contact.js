8a7573a932de45e74bf2e6839202d7b8db64c5a1
Ariel <ariel@pchara.com>
2017-05-04 10:46:36 -0300

    layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,18 +2,26 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 
+const GitHubLink = ({username, children}) => (
+    <span>
+        <i className="fa fa-github"></i> &nbsp;
+        <a href={ "https://github.com/" + username } target="_blank">{children}</a>
+    </span>
+)
 
-const ContactEmail = ({email}) => (
-    <div className="contact-email">
-       <a href={'mailto:' + email}>{email}</a>
-    </div>
+
+const ContactEmailLink = ({email}) => (
+       <span>
+        <span className="icon"> @ </span> &nbsp; 
+        <a href={'mailto:' + email}>{email}</a>
+       </span>
 );
-
-
 
 const Contact = ({contact}) => (
     <div className="contact">
-        <ContactEmail email={contact.email}/>
+        <ContactEmailLink email={contact.email}/>
+        <br/>
+        <GitHubLink username="arielpchara">https://github.com/arielpchara</GitHubLink>
     </div>
 );
 
